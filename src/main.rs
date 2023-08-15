5c164998af833ccf2b086fd063022d8fc2060c6d
i25ma <25mateam@gmail.com>
2023-08-15 10:43:52 +0800

    add systems

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,31 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
+    .add_systems(Update, hello_world)
     .run();
 }
 
 
+// components
+#[derive(Component)]
+struct Position { x: f32, y: f32 }
 
+
+// systems : normal rust funcitons
+
+fn print_position_system(query: Query<&Transform>) {
+    for transform in &query {
+        println!("position: {:?}", transform.translation);
+    }
+}
+
+// entities : a simple type containing a unique integer
+
+struct Entity(u64);
+
+
+// your first system
+
+fn hello_world() {
+    println!("hello world");
+}
